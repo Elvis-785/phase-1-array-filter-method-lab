0b727fde88050ebf56302b67c72f909ccabf8b89
Elvis-785 <kiruielvis3358@gmail.com>
2024-11-01 12:21:43 +0300

    the filter method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+findMatching(drivers, "Bobby");
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+fuzzyMatch(drivers, "Sa")
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
